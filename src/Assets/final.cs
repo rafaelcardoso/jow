07fd69cfe4a6c34978c1387e240408609f660144
Rafael Cardoso <rafaelcardoso.dev@gmail.com>
2013-04-26 12:39:46 -0300

    Visual changes
    Stones were added on top of the first stage.
    Torches were added at some points of the first stage.
    Danger plates were placed in dangerous places to indicate to the player
    that he may die.
    Was added to an entrance of the cave at the end of the first stage.
    In the end of the first phase the screen darkens gradually.

--- src/Assets/final.cs
+++ src/Assets/final.cs
@@ -3,20 +3,40 @@ using System.Collections;
 
 public class final : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public Texture black;
+
+	float alpha = 0f;
+	bool end = false;
 	
+	// Use this for initialization
+	void OnGUI () {
+		if(end){
+			Color color = GUI.color;
+			color.a = this.alpha;
+			GUI.color = color;
+			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), black);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if(end){
+			alpha = Mathf.Clamp01(alpha + 1 * Time.deltaTime);
+			
+			if(alpha == 1f){
+				Application.LoadLevel("menu");
+			}
+		}
+	
+	
+	
 	}
 	
 	void OnTriggerEnter(Collider c){
 		
-		
-			Application.LoadLevel("menu");		
+			end = true;
+			
 		
 		
 	}
